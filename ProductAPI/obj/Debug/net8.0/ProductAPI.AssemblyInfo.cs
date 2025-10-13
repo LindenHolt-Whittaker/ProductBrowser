f990eb8f6bc8418962422436e169009b67bb6b22
Linden Holt-Whittaker <36598942+LindenHolt-Whittaker@users.noreply.github.com>
2025-10-13 11:38:47 +0100

    ProductAPI: tests template

--- ProductAPI/obj/Debug/net8.0/ProductAPI.AssemblyInfo.cs
+++ ProductAPI/obj/Debug/net8.0/ProductAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7b5e97fc9b64aac41a262ea064bb02c3377ec9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
